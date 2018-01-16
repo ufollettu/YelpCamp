97c9e92f74c93a1d231ba1bf768bda9d10341175
Pasquale Merolle <pasquale.merolle@gmail.com>
2018-01-16 10:02:47 +0100

    v8 start

---
+++ v8/routes/comments.js
@@ -0,0 +1,71 @@
+var express = require("express");
+var router = express.Router({mergeParams: true}); // {mergeParams: true} to pass the campground :id to the comment routes
+
+var Campground = require("../models/campground"),
+    Comment = require("../models/comment");
+
+// New
+router.get('/new', isLoggedIn, function(req,res) {
+    Campground.findById(req.params.id, function (err, campground) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('comments/new', {campground:campground});
+        }
+    });
+});
+
+//Create--> with referenced Schema
+router.post("/", isLoggedIn, function (req, res) {
+    Comment.create(req.body.comment, function (err, comment) {
+        if (err) {
+            console.log(err);
+        } else {
+            Campground.findOne({"_id": req.params.id})
+                .populate("comments")
+                .exec(function (err, campground) {
+                    if (err) {
+                        console.log(err);
+                        res.redirect("/campgrounds");
+                    } else {
+                        campground.comments.push(comment);
+                        campground.save();
+                        res.redirect("/campgrounds/" + campground._id);
+                    }
+                });
+        }
+    });
+});
+
+// //Create--> with embedded Schema
+// router.post("/", function (req, res) {
+//     // find the correct campground
+//     Campground.findById(req.params.id, function (err, campground) {
+//         if (err) {
+//             console.log(err);
+//             res.redirect("/campgrounds");
+//         } else {
+//             Comment.create(req.body.comment, function (err, comment) {
+//                 if (err) {
+//                     console.log(err);
+//                 } else {
+//                     campground.comments.push(comment);
+//                     campground.save();
+//                     res.redirect("/campgrounds/" + campground._id);
+//                     // console.log(req.body.comment);
+//                     // console.log(campground.comments);
+//                 }
+//             });
+//         }
+//     });
+// });
+
+//passport middleware
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+module.exports = router;
